cf51cf0e5cd21488c75f15f4aaba5301090ade54
Wesley <wesleycariutaba@gmail.com>
2021-08-18 14:01:40 -0300

    otimização

--- crivo.c
+++ crivo.c
@@ -9,48 +9,66 @@ int main(){
     long int i, j;
     int *table = (int *) malloc(TAMANHO * sizeof(int));
     long int raiz = floor(sqrt(TAMANHO));
+    int qtdPrimos = 0;
 
     
 
     //execução serial
-    for(i = 2;i <= TAMANHO;i++){
-        table[i] = 1;
-    }
-
     double inicio = omp_get_wtime();
-    for(i = 2; i <= raiz;i++){
-        if(table[i]==1){
-            for(j= i+i;j<=TAMANHO;j+=i){
-                table[j] = 0;
-            }
+    #pragma omp parallel num_threads(1)
+    {
+        #pragma omp for
+        for(i = 2;i <= TAMANHO;i++){
+            table[i] = 1;
         }
+
+        #pragma omp for schedule(dynamic)   
+        for(i = 2; i <= raiz;i++){
+            if(table[i]==1){
+                for(int j= i*i;j<=TAMANHO;j+=i){
+                    table[j] = 0;
+                }
+            }
+         }
+
+        #pragma omp parallel for reduction(+:qtdPrimos)
+        for (int i = 2; i <= TAMANHO; i++)
+            qtdPrimos += table[i];
     }
     double fim = omp_get_wtime();
+    
+
     double t_serial = fim-inicio;
     printf("Execucao serial: %f\n",t_serial);
+    printf("Primos:%d \n",qtdPrimos);
 
     //execução paralela
-    for(i = 2;i <= TAMANHO;i++){
-        table[i] = 1;
-    }
-
+    qtdPrimos = 0;
     inicio = omp_get_wtime();
     #pragma omp parallel num_threads(4)
     {
-        
+        #pragma omp for
+        for(i = 2;i <= TAMANHO;i++){
+            table[i] = 1;
+        }
+        #pragma omp for schedule(dynamic)  
         for(i = 2; i <= raiz;i++){
-            if(table[i]==1){
-                #pragma omp for
-                for(int j= i+i;j<=TAMANHO;j+=i){
+            if(table[i]==1){ 
+                for(int j= i*i;j<=TAMANHO;j+=i){
                     table[j] = 0;
                 }
             }
         }
+
+        #pragma omp for reduction(+:qtdPrimos)
+        for (int i = 2; i <= TAMANHO; i++)
+            qtdPrimos += table[i];
     }
     fim = omp_get_wtime();
 
     double t_paralelo = fim - inicio;
 	printf("Execucao paralela: %f\n",t_paralelo);
+    printf("Primos:%d \n",qtdPrimos);
 	double speedup = t_serial/t_paralelo;
 	printf("Speedup: %.4f\n", t_serial/t_paralelo);
 	printf("Eficiencia: %.4f\n",speedup/4.0);
