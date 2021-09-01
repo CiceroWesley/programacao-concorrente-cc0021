7eb663b9cb0a38008dc6ac0fe8e9b44495762100
Wesley <wesleycariutaba@gmail.com>
2021-09-01 12:26:42 -0300

    MPI update

--- CrivoMPI.c
+++ CrivoMPI.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
-
-#define TAMANHO 100
-#define SUBTAMANHO 100
+#include <math.h>
 
 int main(){
 
@@ -12,65 +10,49 @@ int main(){
     MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Status status;
 
     double inicio_proc,fim_proc,tempo_proc;
     long int i;
-    int *subvetor = NULL;
+    long int tamanho, subtamanho;
+    tamanho = 100000000;
+    subtamanho = (int)tamanho/nprocs;
     int *vetor = NULL;
+    long int raiz = floor(sqrt(tamanho));
 
-    if (rank == 0){
-        vetor = (int *)malloc(TAMANHO*sizeof(int));
-        for(long int i = 0;i<TAMANHO;i++){
-            vetor[i] = 1;
-            //printf("%d",vetor[i]);
-        }
-
-    }
+    MPI_Barrier(MPI_COMM_WORLD);
     if (rank==0) {
         inicio_proc = MPI_Wtime();
     }
-    subvetor = (int *)malloc(SUBTAMANHO*sizeof(int));
-    MPI_Scatter(&vetor,SUBTAMANHO,MPI_INT,&subvetor,SUBTAMANHO,MPI_INT,0,MPI_COMM_WORLD);
+    vetor = (int *)malloc(tamanho*sizeof(int));
 
+    for(int i=0;i<subtamanho;i++){
+        vetor[i] = 1;
+    }  
 
-    for(long int i = 2;i<SUBTAMANHO;i++){
-        if(subvetor[i] == 1){
-            for(long int j=i+i;j<SUBTAMANHO;j+=i){
-                subvetor[j] = 0;
+    for(long int i = 2;i<raiz;i++){
+        if(vetor[i] == 1){
+            for(long int j=i*i;j<subtamanho;j+=i){
+                vetor[j] = 0;
             }
         }
 
     }
-    /*
-    for(long int i = 2;i<SUBTAMANHO;i++){
-        printf("%d",subvetor[i]);
-    }
-*/
-    int *resultados = (int *)malloc(TAMANHO*sizeof(int));
-    MPI_Gather(&subvetor,1,MPI_INT,&resultados,1,MPI_INT,0,MPI_COMM_WORLD);
-
-   // for(long int i = 2;i<SUBTAMANHO;i++){
-   //     printf("%d e %ld",resultados[i],i);
-   // }
-//
 
     if (rank == 0){
         fim_proc = MPI_Wtime();
         tempo_proc = fim_proc-inicio_proc;
         long int i;
         long int count = 0;
-        for(i=2;i<TAMANHO;i++){
-            printf("%d",resultados[i]);
-            if(resultados[i]==1){
+        for(i=2;i<tamanho;i++){
+            if(vetor[i]==1){
                 count++;
             }
         }
-        printf("Tempo de processamento: %f\n %ld",tempo_proc, count);
-        printf("PRIMOS:%ld",count);
+        printf("Tempo de processamento: %f\n",tempo_proc);
+        printf("PRIMOSOIIII:%ld",count);
 
     }
-
+    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
     return 0;
 }
